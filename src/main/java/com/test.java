0b188733c4b990822f0d89acbf42c79d743dcea6
lishuang <1192095876@qq.com>
2020-12-14 17:25:55 +0800

    测试test

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -3,5 +3,6 @@ package com;
 public class test {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("args = " + args);
     }
 }
