77acc86459b86dbbffb30e4ddf8bbc27cb1d4653
ciallo <867921617@qq.com>
2020-12-14 19:42:19 +0800

    111

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("args = " + args);
         System.out.println("args = " + args);
         System.out.println("args = " + args);
+        System.out.println("true = " + true);
     }
 }
