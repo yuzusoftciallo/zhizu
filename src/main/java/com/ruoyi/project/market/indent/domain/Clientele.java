4029ee64dcdcecd5e349a9d9ffd092c240f437aa
13712281298 <wei364488583@163.com>
2020-12-16 09:24:43 +0800

    销售表实体类

---
+++ src/main/java/com/ruoyi/project/market/indent/domain/Clientele.java
@@ -0,0 +1,21 @@
+package com.ruoyi.project.market.indent.domain;
+
+import lombok.Data;
+
+/**
+ *
+ *
+ *
+ *
+ */
+@Data
+public class Clientele {
+
+    private  Integer cId;
+    private  String cName;
+    private  String cPhone;
+    private  String cRemark;
+    private  String cCompany;
+    private  String cType;
+
+}
